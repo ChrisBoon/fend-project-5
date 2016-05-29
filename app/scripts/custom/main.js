43154b8b3be72ae427ab89a6fb897fa084f6c1d3
ChrisBoon <cboon27@gmail.com>
2016-05-29 02:53:41 -0400

    Moved 'map' js into viewmodel
    Signed-off-by: ChrisBoon <cboon27@gmail.com>

--- app/scripts/custom/main.js
+++ app/scripts/custom/main.js
@@ -75,25 +75,14 @@ var model = {
   ]
 };
 
-// map is a global variable for easy manipulation
-//// on init we use the Google Mpas API to create a map on this variable
-var map;
 
-// initMap function
-//// options are hard-coded - consider moving out into a general Init command
-function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 15,
-    center: {lat: 40.7202, lng: -74.0453}
-  });
-  // applying bindings after map is initiated as we use Map API functions in the Knockout code
-  ko.applyBindings(new ViewModel());
-}
 
-// Reusable function used to turn each place in my JSON to a knockout observable
-var aPlace = function(data) {
-  this.title = ko.observable(data.title);
+//function to remove all spaces and turn a string to lower case:
+var simplifyTitle = function(str){
+  return str.toLowerCase().replace(/\s+/g, '');
 };
+
+
 
 // app viewModel
 var ViewModel = function(){
@@ -101,10 +90,8 @@ var ViewModel = function(){
   // create alias for easy referencing:
   var self = this;
 
-  //function to remove all spaces and turn a string to lower case:
-  this.simplifyTitle = function(str){
-    return str.toLowerCase().replace(/\s+/g, '');
-  };
+  // set up variable that the map will be stored on:
+  var map = null;
 
   // Set up an observable array for storing each place observable:
   this.allPlaces = ko.observableArray([]);
@@ -113,51 +100,92 @@ var ViewModel = function(){
   //// On load we have an empty string.
   this.searchFor = ko.observable("");
 
-  // function to take JSON places and add as observables into this.allPlaces observable array
-  //TODO: move into an init function
-  model.myPlaces.forEach(function(thisPlace){
-    self.allPlaces.push( new aPlace(thisPlace));
-  });
+  // Reusable function used to turn each place in my JSON to a knockout observable
+  this.aPlace = function(data) {
+    this.title = ko.observable(data.title);
+  };
 
-  // function to run through the this.allPlaces observable array and create a Map Marker for each one
-  this.allPlaces().forEach(function(myItem,i){
-    var marker = new google.maps.Marker({
-      position: {lat: model.myPlaces[i].lat, lng: model.myPlaces[i].lng},
-      map: map,
-      title: model.myPlaces[i].title
-    });
-    myItem.marker = marker;
-  });
-
-  // function for filtering markers and list items based on search field
-  this.filteredItems = ko.computed(function() {
-    var filter = self.simplifyTitle(this.searchFor());
-    // if search field is empty:
-    if (!filter) {
-      //for each item in this.allPlaces:
-      return ko.utils.arrayFilter(this.allPlaces(), function(item) {
-        //make marker visible
-        item.marker.setVisible(true);
-        // keep in list
-        return true;
-      });
-    }
-    // if search field is NOT empty, for each item in this.allPlaces:
-    return ko.utils.arrayFilter(this.allPlaces(), function(item) {
-      // get the title and see if it is in the string:
-      if (self.simplifyTitle( item.title()).indexOf(filter) > -1) {
-        // If it is we make Marker visible and keep in list
-        item.marker.setVisible(true);
-        return true;
-      } else {
-        //if it is NOT we make Marker invisible and remove name from list
-        item.marker.setVisible(false);
-        return false;
+  this.update = function(){
+    // function for filtering markers and list items based on search field
+    this.filteredItems = ko.computed(function() {
+      var filter = simplifyTitle(self.searchFor());
+      // if search field is empty:
+      if (!filter) {
+        //for each item in this.allPlaces:
+        return ko.utils.arrayFilter(self.allPlaces(), function(item) {
+          //make marker visible
+          item.marker.setVisible(true);
+          // keep in list
+          return true;
+        });
       }
+      // if search field is NOT empty, for each item in this.allPlaces:
+      return ko.utils.arrayFilter(self.allPlaces(), function(item) {
+        // get the title and see if it is in the string:
+        if (simplifyTitle( item.title()).indexOf(filter) > -1) {
+          // If it is we make Marker visible and keep in list
+          item.marker.setVisible(true);
+          return true;
+        } else {
+          //if it is NOT we make Marker invisible and remove name from list
+          item.marker.setVisible(false);
+          return false;
+        }
+      });
+
     });
 
-  }, this);
+  };
 
+  // function to create the map:
+  this.createMap = function(){
+    self.map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 15,
+      center: {lat: 40.7202, lng: -74.0453}
+    });
+  };
 
+  this.createList = function(){
+    // function to take JSON places and add as observables into this.allPlaces observable array
+    model.myPlaces.forEach(function(thisPlace){
+      self.allPlaces.push( new self.aPlace(thisPlace));
+    });
+    // function to run through the this.allPlaces observable array and create a Map Marker for each one
+    self.allPlaces().forEach(function(myItem,i){
+      var marker = new google.maps.Marker({
+        position: {lat: model.myPlaces[i].lat, lng: model.myPlaces[i].lng},
+        map: self.map,
+        title: model.myPlaces[i].title
+      });
+      myItem.marker = marker;
+    });
+
+  };
+
+  this.init = function(){
+    self.createMap();
+    self.createList();
+    self.update();
+
+  };
+  //call init
+  this.init();
 };
 
+var initApp = function(){
+  // applying bindings after map is initiated as we use Map API functions in the Knockout code
+  ko.applyBindings(new ViewModel());
+};
+
+//foursquare testing:
+
+var CLIENT_ID = 'MNLX5UCWNCVOONDL1JUOVCPPTJD1V4UTATUBYCEL5VACWBC3';
+var CLIENT_SECRET = 'GMKFEK4K1GT34QW1D1YVLRBWDK0KJRG3JT5VT14G4HIS10FN';
+var venueId = '54fb1694498e727051f154cc';
+var API_ENDPOINT = 'https://api.foursquare.com/v2/venues/' + venueId + '?&client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&v=20160529';
+$.getJSON(API_ENDPOINT, function(result, status) {
+
+
+    console.log(status);
+    console.log(result.response.venue.name);
+});

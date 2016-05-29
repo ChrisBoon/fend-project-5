c9d069b99bc4e9b4200f16fc869909e3b9532e84
ChrisBoon <cboon27@gmail.com>
2016-05-28 23:08:29 -0400

    Add comments to JS code
    Signed-off-by: ChrisBoon <cboon27@gmail.com>

--- app/scripts/custom/main.js
+++ app/scripts/custom/main.js
@@ -1,3 +1,5 @@
+// My list of places
+//// Eventually move to an AJAX call
 var model = {
   myPlaces: [
     {
@@ -73,32 +75,51 @@ var model = {
   ]
 };
 
+// map is a global variable for easy manipulation
+//// on init we use the Google Mpas API to create a map on this variable
 var map;
 
+// initMap function
+//// options are hard-coded - consider moving out into a general Init command
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
     center: {lat: 40.7202, lng: -74.0453}
   });
+  // applying bindings after map is initiated as we use Map API functions in the Knockout code
   ko.applyBindings(new ViewModel());
 }
 
+// Reusable function used to turn each place in my JSON to a knockout observable
 var aPlace = function(data) {
   this.title = ko.observable(data.title);
 };
 
-
+// app viewModel
 var ViewModel = function(){
+
+  // create alias for easy referencing:
   var self = this;
 
+  //function to remove all spaces and turn a string to lower case:
+  this.simplifyTitle = function(str){
+    return str.toLowerCase().replace(/\s+/g, '');
+  };
+
+  // Set up an observable array for storing each place observable:
   this.allPlaces = ko.observableArray([]);
 
-  this.filter = ko.observable("");
+  // filter is bound to the search input.
+  //// On load we have an empty string.
+  this.searchFor = ko.observable("");
 
+  // function to take JSON places and add as observables into this.allPlaces observable array
+  //TODO: move into an init function
   model.myPlaces.forEach(function(thisPlace){
     self.allPlaces.push( new aPlace(thisPlace));
   });
 
+  // function to run through the this.allPlaces observable array and create a Map Marker for each one
   this.allPlaces().forEach(function(myItem,i){
     var marker = new google.maps.Marker({
       position: {lat: model.myPlaces[i].lat, lng: model.myPlaces[i].lng},
@@ -108,21 +129,28 @@ var ViewModel = function(){
     myItem.marker = marker;
   });
 
-
+  // function for filtering markers and list items based on search field
   this.filteredItems = ko.computed(function() {
-    var filter = this.filter().toLowerCase();
+    var filter = self.simplifyTitle(this.searchFor());
+    // if search field is empty:
     if (!filter) {
+      //for each item in this.allPlaces:
       return ko.utils.arrayFilter(this.allPlaces(), function(item) {
-          item.marker.setVisible(true);
-              return true;
+        //make marker visible
+        item.marker.setVisible(true);
+        // keep in list
+        return true;
       });
     }
-
+    // if search field is NOT empty, for each item in this.allPlaces:
     return ko.utils.arrayFilter(this.allPlaces(), function(item) {
-      if (item.title().toLowerCase().indexOf(filter) > -1) {
+      // get the title and see if it is in the string:
+      if (self.simplifyTitle( item.title()).indexOf(filter) > -1) {
+        // If it is we make Marker visible and keep in list
         item.marker.setVisible(true);
         return true;
       } else {
+        //if it is NOT we make Marker invisible and remove name from list
         item.marker.setVisible(false);
         return false;
       }
